3237c76c8fac144e735d56a10809d7edb4f3a258
wenhu1024 <1340773297@qq.com>
2024-01-12 21:25:19 +0800

    lstat_stat_test

---
+++ linux_exercise/file_io_test/lstat_stat_test/stat_test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+
+int main(int argc,char * argv[]){
+    struct stat sb;
+    int ret=stat(argv[1],&sb);
+    if(ret==-1){
+        perror("stat");
+        exit(1);
+    }
+
+    if(S_ISREG(sb.st_mode)){
+        printf("It is a regular.\n");
+    }else if(S_ISDIR(sb.st_mode)){
+        printf("It is a dir.\n");
+    }else if(S_ISFIFO(sb.st_mode)){
+        printf("It is a pipe.\n");
+    }else if(S_ISLNK(sb.st_mode)){
+        printf("It is a sym link.\n");
+    }
+
+    return 0;
+}

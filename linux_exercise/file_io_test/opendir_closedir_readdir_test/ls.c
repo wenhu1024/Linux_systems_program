86562af278639ae54d479ebbf955b98fb0645550
wenhu1024 <1340773297@qq.com>
2024-01-13 00:21:10 +0800

    ls_test

---
+++ linux_exercise/file_io_test/opendir_closedir_readdir_test/ls.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc,char* argv[]){
+    DIR* dir;
+    dir=opendir(argv[1]);
+    if(dir==NULL){
+        perror("opendir error");
+        exit(1);
+    }
+    struct dirent *sdp;
+
+    while((sdp=readdir(dir))!=NULL){
+        if(sdp->d_name[0]=='.'){
+            continue;
+        }
+        printf("%s\t",sdp->d_name);
+    }
+    printf("\n");
+    closedir(dir);
+    return 0;
+}

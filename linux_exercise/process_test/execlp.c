9e327c22ec7f28a7def82d9f58a8b8cf2ec7c5f3
wenhu1024 <1340773297@qq.com>
2024-01-31 17:11:19 +0800

    exec

---
+++ linux_exercise/process_test/execlp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+
+int main(){
+    pid_t pid;
+    printf("start from parent...\n");    
+    pid=fork();
+
+    if(pid<0){
+        perror("fork");
+        exit(1);
+    }else if(pid==0){
+        execlp("ls","ls","-l","-h",NULL);
+        perror("execlp");
+        exit(1);
+    }else if(pid>0){
+        printf("parent pid:%d\n",getpid());
+    }
+
+    return 0;
+}

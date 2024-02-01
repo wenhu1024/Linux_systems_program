7952ad236d688821e5fd5e9546abbb1b249cbdc0
wenhu1024 <1340773297@qq.com>
2024-02-01 18:12:11 +0800

    exec

---
+++ linux_exercise/process_test/exec_test/execvp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc,char *argv[]){
+    pid_t pid;
+    printf("start from parent...\n");    
+    pid=fork();
+
+    if(pid<0){
+        perror("fork");
+        exit(1);
+    }else if(pid==0){
+         char *new_argv[4] = {"ls", "-l", "-h", NULL};
+         new_argv[0]="123";
+        execvp("ls",new_argv);
+        perror("execlp");
+        exit(1);
+    }else if(pid>0){
+        printf("parent pid:%d\n",getpid());
+    }
+}

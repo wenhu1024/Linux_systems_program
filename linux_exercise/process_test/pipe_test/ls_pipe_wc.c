9ef7f8c203be6360f822d97d7f792bbb01060c9d
wenhu1024 <1340773297@qq.com>
2024-02-06 18:00:37 +0800

    ls_pipe_wc

--- linux_exercise/process_test/pipe_test/ls_pipe_wc.c
+++ linux_exercise/process_test/pipe_test/ls_pipe_wc.c
@@ -4,31 +4,41 @@
 #include <sys/types.h>
 #include <errno.h>
 
-void sys_err(const char* str){
+void sys_err(const char *str)
+{
     perror(str);
     exit(1);
 }
 
-int main(){
+int main()
+{
     int fd[2];
     int ret;
     pid_t pid;
 
-    ret=pipe(fd);
-    if(ret==-1){
+    ret = pipe(fd);
+    if (ret == -1)
+    {
         sys_err("pipe");
     }
 
-    pid=fork();
-    if(pid>0){
-        close(fd[0]);
-        ret=dup2(fd[1],STDOUT_FILENO);
-        execlp("ls","ls",NULL);
-        sys_err("execlp");
-    }else if(pid==0){
+    pid = fork();
+    if (pid == -1)
+    {
+        sys_err("fork");
+    }
+    if (pid > 0)
+    {
         close(fd[1]);
-        dup2(fd[0],STDIN_FILENO);
-        execlp("wc","wc","-l",NULL);
+        dup2(fd[0], STDIN_FILENO);
+        execlp("wc", "wc", "-l", NULL);
+        sys_err("execlp");
+    }
+    else if (pid == 0)
+    {
+        close(fd[0]);
+        ret = dup2(fd[1], STDOUT_FILENO);
+        execlp("ls", "ls", NULL);
         sys_err("execlp");
     }
     return 0;

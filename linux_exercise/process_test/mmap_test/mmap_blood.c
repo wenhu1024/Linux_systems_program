21ce8775c84a2f57d997fd2c5c87cd7e9b6ca9ce
wenhu1024 <1340773297@qq.com>
2024-02-08 23:14:27 +0800

    mmap

---
+++ linux_exercise/process_test/mmap_test/mmap_blood.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void sys_err(const char *str){
+    perror(str);
+    exit(1);
+}
+
+int var=1;
+
+int main(){
+    int fd,ret,len;
+    pid_t pid;
+    int *p;
+
+    fd=open("blood_test",O_RDWR|O_CREAT|O_TRUNC,0666);
+    if(fd==-1){
+        sys_err("open");
+    }
+
+    len=20;
+    ftruncate(fd,len);
+    p=mmap(NULL,len,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    if(p==MAP_FAILED){
+        perror("mmap");
+    }
+    close(fd);
+
+    pid=fork();
+    if(pid==-1){
+        sys_err("fork");
+    }
+    if(pid==0){
+        *p=100;
+        var=2;
+        printf("child: *p=%d  var=%d\n",*p,var);
+    }else{
+        sleep(1);
+        printf("parent: *p=%d  var=%d\n",*p,var);
+    }
+
+
+    return 0;
+}

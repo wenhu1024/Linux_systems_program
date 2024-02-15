4338b6cae7d5413a2fba7f64e85437be96a340ff
wenhu1024 <1340773297@qq.com>
2024-02-15 14:40:34 +0800

    no blood

--- linux_exercise/process_test/mmap_test/mmap_r.c
+++ linux_exercise/process_test/mmap_test/mmap_r.c
@@ -5,7 +5,8 @@
 #include <unistd.h>
 #include <string.h>
 
-void sys_err(const char *str){
+void sys_err(const char *str)
+{
     perror(str);
     exit(1);
 }
@@ -17,34 +18,35 @@ struct student
     int age;
 };
 
-int main(){
-    int fd,ret,len;
+int main()
+{
+    int fd, ret, len;
     struct student stu;
-    struct student *p=NULL;
+    struct student *p = NULL;
 
-    len=sizeof(stu);
-    fd=open("test",O_RDWR|O_CREAT|O_TRUNC,0666);
-    if(fd==-1){
+    len = sizeof(stu);
+    fd = open("test", O_RDWR);
+    if (fd == -1)
+    {
         sys_err("open");
     }
 
-    p=mmap(NULL,len,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-    if(p==MAP_FAILED){
+    p = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (p == MAP_FAILED)
+    {
         sys_err("mmap");
     }
     close(fd);
-    while(1){
-        printf("id=%d  age=%d\n",p->id,p->age);
+    while (1)
+    {
+        printf("id=%d  age=%d  name=%s\n", p->id, p->age,p->name);
         sleep(2);
     }
 
-    ret=munmap(p,len);
-    if(ret==-1){
+    ret = munmap(p, len);
+    if (ret == -1)
+    {
         sys_err("munmap");
     }
     return 0;
 }
-
-
-
-
--- linux_exercise/process_test/mmap_test/mmap_w.c
+++ linux_exercise/process_test/mmap_test/mmap_w.c
@@ -6,7 +6,8 @@
 #include <unistd.h>
 #include <string.h>
 
-void sys_err(const char *str){
+void sys_err(const char *str)
+{
     perror(str);
     exit(1);
 }
@@ -18,33 +19,36 @@ struct student
     int age;
 };
 
-int main(){
-    int fd,ret,len;
-    struct student stu={1,"Tom",20};
-    struct student *p=NULL;
+int main()
+{
+    int fd, ret, len;
+    struct student stu = {1, "Tom", 20};
+    struct student *p = NULL;
 
-    len=sizeof(stu);
-    fd=open("test",O_RDWR|O_CREAT|O_TRUNC,0666);
-    if(fd==-1){
+    len = sizeof(stu);
+    fd = open("test", O_RDWR | O_CREAT | O_TRUNC, 0664);
+    if (fd == -1)
+    {
         sys_err("open");
     }
-    ftruncate(fd,len);
-    p=mmap(NULL,len,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-    if(p==MAP_FAILED){
+    ftruncate(fd, len);
+    p = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (p == MAP_FAILED)
+    {
         sys_err("mmap");
     }
     close(fd);
-    // while(1){
+    while (1)
+    {
         ++stu.id;
-        memcpy(p,&stu,len);
-        sleep(10);
-    // }
+        memcpy(p, &stu, len);
+        sleep(1);
+    }
 
-    ret=munmap(p,len);
-    if(ret==-1){
+    ret = munmap(p, len);
+    if (ret == -1)
+    {
         sys_err("munmap");
     }
     return 0;
 }
-
-

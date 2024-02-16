699e4e16732de813dde52074b7ae4aed46af1d09
wenhu1024 <1340773297@qq.com>
2024-02-16 16:56:55 +0800

    sigaction

---
+++ linux_exercise/process_test/signal_test/sigaction.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void func(int signo)
+{
+    if (signo == SIGINT)
+    {
+        printf("\tcatch SIGINT\n");
+        sleep(3);
+    }
+    else if (signo == SIGQUIT)
+    {
+        printf("\tcatch SIGQUIT\n");
+    }
+    return;
+}
+
+int main()
+{
+    struct sigaction act,oldact;
+    act.sa_handler=func;
+    sigemptyset(&(act.sa_mask));
+    act.sa_flags=0;
+
+    int ret;
+    ret=sigaction(SIGINT,&act,&oldact);
+    if(ret==-1){
+        perror("sigaction");
+        exit(1);
+    }
+    ret=sigaction(SIGQUIT,&act,&oldact);
+    if(ret==-1){
+        perror("sigaction");
+        exit(1);
+    }    
+    while (1)
+    {
+        ;
+    }
+
+    return 0;
+}

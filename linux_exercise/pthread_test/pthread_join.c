f3eca6bec21b0101907602c46b77cdf6986f5023
wenhu1024 <1340773297@qq.com>
2024-02-19 22:49:56 +0800

    pthread_join

---
+++ linux_exercise/pthread_test/pthread_join.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+struct test{
+    int n;
+    char str[256];
+};
+
+void *fcn(void *arg){
+    struct test *t=(struct test*)malloc(sizeof(struct test));
+    t->n=100;
+    strcpy(t->str,"just test");
+    pthread_exit((void*)t);
+}
+
+void sys_err(const char* str){
+    perror(str);
+    exit(1);
+}
+
+int main(){
+    int ret;
+    pthread_t tid;
+    struct test *pt;
+
+    ret=pthread_create(&tid,NULL,fcn,NULL);
+    if(ret!=0){
+        sys_err("pthread_create");
+    }
+
+    ret=pthread_join(tid,(void**)&pt);
+    if(ret!=0){
+        sys_err("pthread_join");
+    }
+
+    printf("%d      %s\n",pt->n,pt->str);
+    pthread_exit((void*)0);
+}
